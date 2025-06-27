8630c12d4e561121b6c0807e180c07c58bc91a14
Leyson Castaño <leysofcp@hotmail.com>
2025-06-27 14:33:56 -0500

    Update global.js

--- src/config/global.js
+++ src/config/global.js
@@ -207,7 +207,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/52450481_CF03_DU.pdf',
       },
       {
         icono: 'fas fa-download',
